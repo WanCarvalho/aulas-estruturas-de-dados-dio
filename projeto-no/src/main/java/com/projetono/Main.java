623f70ac5bb2420f6751e0e5dff3b6d44fee9fa5
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2022-02-20 18:40:34 -0300

    Refatoração da classe Nó utilizando generics

--- projeto-no/src/main/java/com/projetono/Main.java
+++ projeto-no/src/main/java/com/projetono/Main.java
@@ -5,19 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        No no1 = new No("Conteúdo no1");
+        No<String> no1 = new No<>("Conteúdo no1");
 
-        No no2 = new No("Conteúdo no2");
+        No<String> no2 = new No<>("Conteúdo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<>("Conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<>("Conteúdo no4");
         no3.setProximoNo(no4);
 
         //apontamento de Nó's(encadeamento): no1->no2->no3->no4->null
-
         System.out.println(no1);
         System.out.println(no1.getProximoNo());
 
