c3bdce03f7c756ca8ebfa8929617896e19c028e0
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2022-02-23 21:24:27 -0300

    Adição da classe fila

---
+++ fila/src/main/java/one/digital/innovation/Fila.java
@@ -0,0 +1,74 @@
+package one.digital.innovation;
+
+public class Fila {
+
+    private No refNoEntradaFila;
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+    }
+
+    public void enqueue(No novoNo){
+        novoNo.setRefNo(refNoEntradaFila);//novoNo recebe a ref de entrada da fila
+        refNoEntradaFila = novoNo;//ref de entrada recebe novoNo que passa a ser o primeiro da fila
+    }
+
+    public No first(){
+        if (!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            while(true){//looping até o método chegar ao primeiro nó da fila
+                if (primeiroNo.getRefNo() != null){
+                    primeiroNo = primeiroNo.getRefNo();
+                }else{
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public No dequeue(){
+        if (!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while(true){//looping até o método chegar ao primeiro nó da fila
+                if (primeiroNo.getRefNo() != null){
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                }else{
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return refNoEntradaFila = null? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        No noAuxiliar = refNoEntradaFila;
+
+        if (refNoEntradaFila != null){
+            while (true) {
+                stringRetorno += '[No{objeto=' + noAuxiliar.getObject() + "]}--->";
+                noAuxiliar = noAuxiliar.getRefNo();
+                if (noAuxiliar.getRefNo() != null){
+                    noAuxiliar = noAuxiliar.getRefNo();
+                }else{
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+        }else{
+            stringRetorno = "null";
+        }
+
+        return stringRetorno;
+    }
+}
