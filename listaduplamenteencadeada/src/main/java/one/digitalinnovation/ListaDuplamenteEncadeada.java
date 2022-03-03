4442eaf4edaeb2ab7970f12178a540f5b2adce1e
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2022-03-02 21:17:33 -0300

    implementação dos métodos getNo() e get(). Aproveitando o código do método getNo() para o método get()

--- listaduplamenteencadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ listaduplamenteencadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -14,6 +14,22 @@ public class ListaDuplamenteEncadeada<T> {
         this.tamanhoLista = 0;
     }
 
+    //pega o conteudo do nó referente ao index/índice passado
+    public T get(int index){
+        return this.getNo(index);
+    }
+
+    //método usado para pegar valor do index/índice passado. o método pecorre a lista até chegar ao index/índice passado
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for (int i =0; (i < index) && (noAuxiliar != null); i++){//diferente de nulo pois a refNo = nulo se refere ao ultimo nó da lista
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
+    //retorna tamanho da lista duplamente encadeada
     public int size(){
         return tamanhoLista;
     }
